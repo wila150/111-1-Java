c8a5a22759fd1aba3f1a69343131820bef24e02b
XieChengFang <114204364+wila150@users.noreply.github.com>
2022-09-23 11:40:27 +0800

    Add files via upload

--- Test.java
+++ Test.java
@@ -2,6 +2,7 @@ public class Test{
     public static void main(String [] args){
            System.out.println(args[0]); 
            System.out.println(args[1]); 
+           System.out.println("Sencond Versionl"); 
  }
 }
 			
