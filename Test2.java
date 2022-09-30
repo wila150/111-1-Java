3f34222af5a7731345e29060f427172994bd4fc9
XieChengFang <114204364+wila150@users.noreply.github.com>
2022-09-30 11:30:11 +0800

    Add files via upload

---
+++ Test2.java
@@ -0,0 +1,10 @@
+public class Test2{
+   public static void main(String [] args){
+	    System.out.println(args[0]); 
+		printStr("XieChengFang!");
+   }
+   public static void printStr(String str){
+           System.out.println("My Name = "+ str); 
+
+   }
+}
