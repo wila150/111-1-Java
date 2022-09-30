343126c8500296db6d7eb4cc56974b77bbb8d847
XieChengFang <114204364+wila150@users.noreply.github.com>
2022-09-30 11:28:36 +0800

    Add files via upload

---
+++ Test3.java
@@ -0,0 +1,20 @@
+public class Test3{
+   public static void main(String [] args){
+	    int num =1;
+        printStr(num);
+        num = 2;
+        printStr(num);
+        num = num + 1 ;
+        printStr(num);
+        num += 1;
+        printStr(num);
+        ++num;
+        printStr(num);
+        num++;
+        printStr(num);
+   }
+   public static void printStr(int str){
+           System.out.println(str); 
+
+   }
+}
