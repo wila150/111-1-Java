9cdf63fca8d99182b9675d1453b4226e702d64aa
XieChengFang <114204364+wila150@users.noreply.github.com>
2022-10-07 09:24:02 +0800

    Test-1-10/7

--- Test6.java
+++ Test6.java
@@ -1,6 +1,6 @@
 //javac -encoding UTF-8 Test5.java
 import java.util.Scanner;
-public class Test5{
+public class Test6{
    public static void main(String [] args){
 	    int grade;
         
@@ -10,11 +10,23 @@ public class Test5{
         System.out.printf("Enter first integer:");
         grade = input.nextInt();
         
-        if (grade >=60 & grade <=70){
-            System.out.println("<=70");
+        if (grade >=90 & grade <=100){
+            System.out.println("A");
         }   
-        else{
-            System.out.println("<60");
+        else if (grade >=80 & grade <=89){
+            System.out.println("B");
+         
         }  
+		else if (grade >=70 & grade <=79){
+            System.out.println("C");
+         
+        } 
+		else if (grade >=60 & grade <=69){
+            System.out.println("D");
+        
+        } 
+		else{
+			System.out.println("E");
+		}
    }
 }
