a260faa755c449f4070ea7902ad33123db4f2a1d
XieChengFang <114204364+wila150@users.noreply.github.com>
2022-10-07 10:19:53 +0800

    Test-防呆-10/7

---
+++ Test8.java
@@ -0,0 +1,35 @@
+//javac -encoding UTF-8 Test5.java
+import java.util.Scanner;
+public class Test8{
+   public static void main(String [] args){
+	    int grade;
+        
+        Scanner input = new Scanner(System.in);
+        
+        
+        System.out.printf("Enter first integer:");
+        grade = input.nextInt();
+     if (grade > 100){ 
+        System.out.println("Please enter again");
+        }     
+	else{
+        switch(grade/10){
+			case 9:
+			case 10:
+					System.out.println("A");
+					break;
+			case 8 :
+					System.out.println("B");
+					break;
+			case 7:
+					System.out.println("C");
+					break;
+			case 6 :
+					System.out.println("D");
+					break; 
+			default :
+					System.out.println("E");
+		}
+    }
+}
+}
