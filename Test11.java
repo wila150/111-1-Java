09feb09fec7ae8d06c55aa24ae08fa278d0caa7a
XieChengFang <114204364+wila150@users.noreply.github.com>
2022-10-14 11:43:11 +0800

    Add files via upload

---
+++ Test11.java
@@ -0,0 +1,24 @@
+public class Test12 {
+	public static void main(String[] args){
+		int row = 0 ;
+		String star = "*";
+		
+		while (row < 5) {
+			int b = 4 - row ;
+			
+			while(b > 0){
+                System.out.print(" ");
+				b = b - 1 ;
+			
+			}
+
+			
+			System.out.println(star);
+			
+			star = star + "**";
+				
+			row = row +1;
+		
+		}
+	}
+}      
